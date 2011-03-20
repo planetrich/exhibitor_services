e33a367a90e33d9a4689a2fa6b78cc7dbd5043f6
Rich Brandwein <rhb@planetconnect.com>
2011-03-20 15:48:48 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExhibitorServices::Application.config.secret_token = 'd59e062a8993ad18501b08a3bdbf2cbed666dd003a5e208d3247c1db11f18d5a464707b2bffa7d089be9ad262a3e366725469b717bcd74dd76c9b16c162df912'
